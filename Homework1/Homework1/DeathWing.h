ae1bd9c05938f34ddcab2fa0a84b1615f38b73c2
Kim HyunWoo <stories2@naver.com>
2017-09-24 01:37:42 +0900

    Main module : change hardcoded variable

--- Homework1/Homework1/DeathWing.h
+++ Homework1/Homework1/DeathWing.h
@@ -2,6 +2,9 @@
 #ifndef ONE_DEATH_WING
 
 #define ONE_DEATH_WING 12
+#define CARD_PID 0
+#define ZERO 0
+#define MAXIMUM_OF_DECK 3
 
 #include <cstdio>
 #include <unistd.h>
--- Homework1/Homework1/main.cpp
+++ Homework1/Homework1/main.cpp
@@ -2,17 +2,17 @@
 
 int main()
 {
-	int pid, programCounter = 0, i;
+	int cardId, cardDrawNum = ZERO, draw;
     printf("hello from Homework1!\n");
-	for (i = 0; i < 10; i += 1) {
-		programCounter += 1;
-		pid = fork();
-		if (pid == 0) {
+	for (draw = ZERO; draw < MAXIMUM_OF_DECK; draw += 1) {
+		cardDrawNum += 1;
+		cardId = fork();
+		if (cardId == CARD_PID) {
 			break;
 		}
 	}
 
 	wait(NULL);
-	printf("my pid: %d prog: %d\n", pid, programCounter);
+	printf("my pid: %d prog: %d\n", cardId, cardDrawNum);
     return 0;
 }
