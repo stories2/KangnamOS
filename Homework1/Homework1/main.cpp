bd234d1b04d8d7721cb199c3a961ac3a9de9c98d
Kim HyunWoo <stories2@naver.com>
2017-09-24 01:20:46 +0900

    Main module : multiple process running

--- Homework1/Homework1/main.cpp
+++ Homework1/Homework1/main.cpp
@@ -4,12 +4,17 @@
 
 int main()
 {
-	int pid;
+	int pid, programCounter = 0, i;
     printf("hello from Homework1!\n");
-
-	pid = fork();
+	for (i = 0; i < 10; i += 1) {
+		programCounter += 1;
+		pid = fork();
+		if (pid == 0) {
+			break;
+		}
+	}
 
 	wait(NULL);
-	printf("my pid: %d\n", pid);
+	printf("my pid: %d prog: %d\n", pid, programCounter);
     return 0;
 }
