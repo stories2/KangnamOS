2e7677c67cb053baeabe54222689005cca7f1920
Kim HyunWoo <stories2@naver.com>
2017-09-24 03:35:49 +0900

    Main module : update log messages

--- Homework1/Homework1/main.cpp
+++ Homework1/Homework1/main.cpp
@@ -2,11 +2,16 @@
 
 int main()
 {
+	SayMalygos("main", "main", "I am deathwing the destroyer the end of all things", LOG_LEVEL_VERBOSE);
 	AttackEnemy(1734, 8);
     return 0;
 }
 
 void AttackEnemy(int damageScale, int attackTimes) {
+	char buffer[200];
+	sprintf(buffer, "Attacking enemy damageScale: %d attackTimes: %d", damageScale, attackTimes);
+	SayMalygos("main", "AttackEnemy", buffer, LOG_LEVEL_INFO);
+
 	int cardId, cardDrawNum = ZERO, draw;
 
 	for (draw = ZERO; draw < MAXIMUM_OF_DECK; draw += 1) {
@@ -31,7 +36,7 @@ void AttackEnemy(int damageScale, int attackTimes) {
 		break;
 	case CARD_DEATH_WING:
 		wait(NULL);
-		SayMalygos("main", "AttackEnemy", "I am deathwing the destroyer the end of all things", LOG_LEVEL_VERBOSE);
+		SayMalygos("main", "AttackEnemy", "The end of game", LOG_LEVEL_INFO);
 		break;
 	default:
 		break;
