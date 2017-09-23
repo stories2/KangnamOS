e4e3626b19cb961e021a1860439202c2b87393d5
Kim HyunWoo <stories2@naver.com>
2017-09-24 01:15:55 +0900

    Main module : fork and wait testing

--- Homework1/Homework1/main.cpp
+++ Homework1/Homework1/main.cpp
@@ -1,7 +1,15 @@
 #include <cstdio>
+#include <unistd.h>
+#include <wait.h>
 
 int main()
 {
+	int pid;
     printf("hello from Homework1!\n");
+
+	pid = fork();
+
+	wait(NULL);
+	printf("my pid: %d\n", pid);
     return 0;
 }
