8cd8ccd50c8010a9b5c0e3e50f282ef407c021be
stories2 <stories2@naver.com>
2017-11-06 16:09:54 +0900

    main loop myshell testing

--- Homework2/Homework2/src/main.cpp
+++ Homework2/Homework2/src/main.cpp
@@ -2,14 +2,21 @@
 
 int main() {
     
-    string argv[] = {"/bin/sh", "-c", "who | wc"}, argv2[] = {"wc"};
-    LogManager *logManager = new LogManager();
-    logManager->PrintLogInfo("main", "main", "logging test");
-    ExecuteManager *manager = new ExecuteManager(*logManager);
-    manager->SetOrder(argv);
-    manager->Execute();
-    delete manager;
-    delete logManager;
+    string argv[] = {"/bin/sh", "-c", "who | wc"};
+    string messageVar;
+    while(1) {
+        LogManager *logManager = new LogManager();
+        logManager->PrintLogInfo("main", "main", "logging test");
+        ExecuteManager *manager = new ExecuteManager(*logManager);
+        cout << "myshell$ ";
+//        cin.ignore();
+        getline(cin, messageVar, '\n');
+        argv[2] = messageVar;
+        manager->SetOrder(argv);
+        manager->Execute();
+        delete manager;
+        delete logManager;
+    }
     return 0;
 }
 
