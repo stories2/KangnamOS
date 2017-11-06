7436bdecc2808c11c9691435bdf8f08293ad57bc
stories2 <stories2@naver.com>
2017-11-06 14:24:49 +0900

    delete comment

--- Homework2/Homework2/src/main.cpp
+++ Homework2/Homework2/src/main.cpp
@@ -2,29 +2,14 @@
 
 int main() {
     
-//    char *argv[] ={"ls", "-al"};
     string argv[] = {"ls", "-al"};
-    /*
-     cout << "make child process" << endl;
-     pid_t pid = fork();
-     if(pid == CHILD_PROCESS) {
-     cout << "child " << pid << endl;
-     execvp(argv[0], argv);
-     }
-     else if(pid > CHILD_PROCESS) {
-     cout << "parent " << pid << endl;
-     }
-     else {
-     cout << "error" << endl;
-     }
-     */
     LogManager *logManager = new LogManager();
     logManager->PrintLogInfo("main", "main", "logging test");
-    delete logManager;
     ExecuteManager *manager = new ExecuteManager(*logManager);
     manager->SetOrder(argv);
     manager->Execute();
     delete manager;
+    delete logManager;
     return 0;
 }
 
