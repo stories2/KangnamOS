1aca9b06e709a765757f15d69d5218e439656e08
stories2 <stories2@naver.com>
2017-11-27 16:39:05 +0900

    Homework3 make new project

---
+++ Homework3/Homework3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Homework3
+//
+//  Created by 김현우 on 2017. 11. 27..
+//  Copyright © 2017년 김현우. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
