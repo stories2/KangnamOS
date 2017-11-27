b87a6e48f7979ea756ac3f3946b7a94157ba7d7f
stories2 <stories2@naver.com>
2017-11-27 20:09:06 +0900

    Setup project structure

--- Homework3/Homework3/main.c
+++ Homework3/Homework3/main.c
@@ -8,8 +8,49 @@
 
 #include <stdio.h>
 
+#define PROCESS_NUM 5
+#define PROCESS_INFO 2
+
+void SimulateFCFS(int [PROCESS_NUM][PROCESS_INFO]);
+void SimulateRoundRobin(int [PROCESS_NUM][PROCESS_INFO]);
+void SimulateSPN(int [PROCESS_NUM][PROCESS_INFO]);
+void SimulateSRT(int [PROCESS_NUM][PROCESS_INFO]);
+void SimulateHRRN(int [PROCESS_NUM][PROCESS_INFO]);
+void SimulateFeedback(int [PROCESS_NUM][PROCESS_INFO]);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int processTestData[PROCESS_NUM][PROCESS_INFO] = {
+        {0, 3},
+        {2, 6},
+        {4, 4},
+        {6, 5},
+        {8, 2}
+    };
+    SimulateFCFS(processTestData);
     return 0;
+}
+
+void SimulateFCFS(int processTestData[PROCESS_NUM][PROCESS_INFO]) {
+    
+}
+
+void SimulateRoundRobin(int processTestData[PROCESS_NUM][PROCESS_INFO]) {
+    
+}
+
+void SimulateSPN(int processTestData[PROCESS_NUM][PROCESS_INFO]) {
+    
+}
+
+void SimulateSRT(int processTestData[PROCESS_NUM][PROCESS_INFO]) {
+    
+}
+
+void SimulateHRRN(int processTestData[PROCESS_NUM][PROCESS_INFO]) {
+    
+}
+
+void SimulateFeedback(int processTestData[PROCESS_NUM][PROCESS_INFO]) {
+    
 }
